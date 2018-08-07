6b92ca19f657132e8f79ae1a7822520e7b0b951a
aneesh631 <aneesh.kundu14@gmail.com>
2018-08-08 00:53:37 +0530

    fixed 413:payload too large

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 3000 ;
 
 const app = express()
 
-app.use(express.json())
+app.use(express.json({limit: '50mb'}))
 app.use(express.urlencoded({extended: true}))
 app.use(session({
     secret: process.env.PASSPORT_SECRET || config.PASSPORT_SECRET,
